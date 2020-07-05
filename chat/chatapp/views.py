f8771bd903ae59fbb2168b4d06956468b0f71f55
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-07-05 20:11:47 +0530

    initial commit

--- chat/chatapp/views.py
+++ chat/chatapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-
 def index(request):
     return render(request, 'index.html')
 
 
 def room(request, room_name):
     return render(request, 'room.html', {'room_name': room_name})
+
+
+
+
